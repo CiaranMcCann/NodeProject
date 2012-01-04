cfa8cceea056522f5eba9bd9009a346725379430
CiaranMcCann <ciaran@flax.ie>
2012-01-04 17:35:31 +0000

    Got a basic test going, using bubble sort in the worker and then one in UI thread. There seesm to be a high enough penatly copying memory from UI thread to a worker. So the job needs to be big for the woker to out preform the UI thread

--- Distrube/task.js
+++ Distrube/task.js
@@ -1,16 +1,30 @@
 self.onmessage = function(event) {
    
-  var img = event.data;
-  var offset = 4;
-  for (var i = 0; i < img.length; i += offset){
-	
-	 img[i]     = 255; // red channel
-	 img[i + 1] = 100; // green channel
-	 img[i + 2] = 0;   // blue channel
-	 img[i + 3] = 255; // alpha channel
-  }
+		var unSortedArray=event.data;
+		var tempStorage=0;
+		var lenght=unSortedArray.length;
+		var indexLenght=lenght-1;
+		var indexPostion=0;
 
-  self.postMessage(img);  
+		for(var i=0;i<indexLenght;i++)
+		{
+			while((indexPostion<indexLenght)){
+			   
+			   if(unSortedArray[indexPostion]>unSortedArray[indexPostion+1])
+			   {
+				   tempStorage=unSortedArray[indexPostion];
+				   unSortedArray[indexPostion]=unSortedArray[indexPostion+1];
+				   unSortedArray[indexPostion+1]=tempStorage
+
+			    }
+
+			    indexPostion++
+			}
+
+		indexPostion=0
+	}
+
+  self.postMessage(unSortedArray);  
 
 };
 
